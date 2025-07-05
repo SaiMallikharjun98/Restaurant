1fd71703957fef28e00264e2f151163a110f58b5
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-07-05 17:01:45 +0530

    styling aded to caurosel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { Card, CardContent } from "@/components/ui/card";
-
 import {
   Carousel,
   CarouselContent,
@@ -12,26 +10,32 @@ import {
 
 function Home({ items }) {
   return (
-    <>
-      <Carousel className="w-full max-w-full ">
-        <CarouselContent className="ml-1">
-          {items.map((item, index) => (
-            <CarouselItem key={index} className="pl-1">
-              <div className="p-1">
-                <Card>
-                  <CardContent className="flex  items-center justify-center p-2">
-                    <img className="w-24 h-32 rounded-full" src={item.image} />
-                  </CardContent>
-                  <h3 className="text-xl text-center ">{item.name}</h3>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-    </>
+    <Carousel className="w-full max-w-full">
+      <CarouselContent className="ml-1 flex">
+        {items.map((item, index) => (
+          <CarouselItem
+            key={index}
+            className="pl-1"
+            style={{ flexBasis: `${100 / items.length}%` }}
+          >
+            <div className="p-1">
+              <Card className="border-0 shadow-none">
+                <CardContent className="flex items-center justify-center p-2">
+                  <img
+                    className="w-24 h-32 rounded-full"
+                    src={item.image}
+                    alt={item.name}
+                  />
+                </CardContent>
+                <h3 className="text-xl text-center">{item.name}</h3>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
 }
 
