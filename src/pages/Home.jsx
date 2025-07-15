0a30c8d43a6ab4b4673838aad755c8154650fc61
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-07-15 21:06:47 +0530

    main section aded

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import image from "../../public/images/background.jpg";
+// import image from "../../public/images/background.jpg";
+import mainBackground from "../../public/images/main-background-image.jpg";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
@@ -7,37 +8,60 @@ import {
   CarouselItem,
 } from "@/components/ui/carousel";
 
-function Home({ items }) {
+function Home() {
   return (
-    <div
-      style={{ backgroundImage: `url(${image})` }}
-      className="h-56 w-full bg-cover relative"
-    >
-      <Carousel className="w-full max-w-full absolute ">
-        <CarouselContent className="ml-1 flex">
-          {items.map((item, index) => (
-            <CarouselItem
-              key={index}
-              className="pl-1"
-              style={{ flexBasis: `${100 / items.length}%` }}
-            >
-              <div className="p-1">
-                <Card className="border-0 shadow-none">
-                  <CardContent className="flex items-center justify-center p-2">
-                    <img
-                      className="w-32 h-32 rounded-full text-white"
-                      src={item.image}
-                      alt={item.name}
-                    />
-                  </CardContent>
-                  <h3 className="text-xl text-center">{item.name}</h3>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
-    </div>
+    <>
+      {/* <div
+        style={{ backgroundImage: `url(${image})` }}
+        className="h-56 w-full bg-cover relative"
+      >
+        <Carousel className="w-full max-w-full absolute ">
+          <CarouselContent className="ml-1 flex">
+            {items.map((item, index) => (
+              <CarouselItem
+                key={index}
+                className="pl-1"
+                style={{ flexBasis: `${100 / items.length}%` }}
+              >
+                <div className="p-1">
+                  <Card className="border-0 shadow-none">
+                    <CardContent className="flex items-center justify-center p-2">
+                      <img
+                        className="w-32 h-32 rounded-full "
+                        src={item.image}
+                        alt={item.name}
+                      />
+                    </CardContent>
+                    <h3 className="text-xl text-center text-white">
+                      {item.name}
+                    </h3>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div> */}
+
+      <main>
+        <div className="w-full relative">
+          <img
+            src={mainBackground}
+            alt="mainImage"
+            className="w-full h-80 bg-cover"
+          />
+          <h1 className="absolute text-center font-bold text-3xl">
+            Welcome to Annapurna Treats Restaurant!
+          </h1>
+          <p className="absolute text-center font-light">
+            Experience delicious flavors, cozy ambiance, and heartfelt
+            hospitality. Enjoy your meal!
+          </p>
+        </div>
+      </main>
+
+      {/* <footer></footer> */}
+    </>
   );
 }
 
