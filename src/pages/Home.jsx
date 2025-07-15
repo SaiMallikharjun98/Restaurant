f39fcce7cc8bc634007986f9c4ec03e93de1c90c
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-07-15 21:18:17 +0530

    main section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,7 +44,7 @@ function Home() {
       </div> */}
 
       <main>
-        <div className="relative h-64 w-full bg-cover bg-center flex  items-center justify-center ">
+        <div className="relative h-64 w-full bg-cover bg-center flex  items-center justify-center pt-4">
           <img
             src={mainBackground}
             alt="mainImage"
@@ -53,7 +53,7 @@ function Home() {
           <h1 className="absolute text-center font-bold mb-2 text-3xl ">
             Welcome to Annapurna Treats Restaurant!
           </h1>
-          <p className="absolute text-center max-w-xl font-light pt-8">
+          <p className="absolute text-center max-w-xl font-light pt-10">
             Experience delicious flavors, cozy ambiance, and heartfelt
             hospitality. Enjoy your meal!
           </p>
