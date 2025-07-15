b85d3c06b82479827fe82545b51d6ece41fd1a96
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-07-15 20:33:21 +0530

    background Image aded to caurosel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import image from "../../public/images/background.jpg";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
@@ -8,30 +9,33 @@ import {
 
 function Home({ items }) {
   return (
-    <Carousel className="w-full max-w-full">
-      <CarouselContent className="ml-1 flex">
-        {items.map((item, index) => (
-          <CarouselItem
-            key={index}
-            className="pl-1"
-            style={{ flexBasis: `${100 / items.length}%` }}
-          >
-            <div className="p-1">
-              <Card className="border-0 shadow-none">
-                <CardContent className="flex items-center justify-center p-2">
-                  <img
-                    className="w-32 h-32 rounded-full"
-                    src={item.image}
-                    alt={item.name}
-                  />
-                </CardContent>
-                <h3 className="text-xl text-center">{item.name}</h3>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-    </Carousel>
+    <div className="w-full relative">
+      <img src={image} alt="backgroundImage" className="h-24 w-full" />
+      <Carousel className="w-full max-w-full">
+        <CarouselContent className="ml-1 flex">
+          {items.map((item, index) => (
+            <CarouselItem
+              key={index}
+              className="pl-1"
+              style={{ flexBasis: `${100 / items.length}%` }}
+            >
+              <div className="p-1">
+                <Card className="border-0 shadow-none">
+                  <CardContent className="flex items-center justify-center p-2">
+                    <img
+                      className="w-32 h-32 rounded-full"
+                      src={item.image}
+                      alt={item.name}
+                    />
+                  </CardContent>
+                  <h3 className="text-xl text-center">{item.name}</h3>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </div>
   );
 }
 
